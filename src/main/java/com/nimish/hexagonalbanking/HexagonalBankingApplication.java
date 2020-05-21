a8274158620585dac92427da29aa3e6e604bf75f
nimish jindal <nimish.jindal@yahoo.com>
2020-05-22 02:40:14 +0530

    started git repo with spring boot initializer code

---
+++ src/main/java/com/nimish/hexagonalbanking/HexagonalBankingApplication.java
@@ -0,0 +1,13 @@
+package com.nimish.hexagonalbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nimish/hexagonalbanking/HexagonalBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimish.hexagonalbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
